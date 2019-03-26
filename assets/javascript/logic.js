f0ce4f04b160385d81fd0b55e95b2b2e95a8ab35
Matthew Taube <taubemj@gmail.com>
2019-03-26 15:56:32 -0400

    basics

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAbikwpbT9TYoTOiyc5gImelzmQrj_Uge8",
+    authDomain: "train-schedule-1a1d0.firebaseapp.com",
+    databaseURL: "https://train-schedule-1a1d0.firebaseio.com",
+    projectId: "train-schedule-1a1d0",
+    storageBucket: "train-schedule-1a1d0.appspot.com",
+    messagingSenderId: "202700202219"
+  };
+  firebase.initializeApp(config);
